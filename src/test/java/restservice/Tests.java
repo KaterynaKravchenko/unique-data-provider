d8f006e6cf99972c23dec3aba50afcde74739002
kateryna Kravchenko <kateryna.kravchenko@clover.tech>
2022-02-04 16:12:25 +0200

    tests for endpoints

---
+++ src/test/java/restservice/Tests.java
@@ -0,0 +1,40 @@
+package restservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class SmokeTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void notEmptyGetResponseTest() throws Exception {
+
+        this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void postMethodTest() throws Exception {
+
+        this.mockMvc.perform((post("/").param("data", "Abstract")))
+                .andDo(print()).andExpect(status().isOk());
+    }
+
+    @Test
+    public void emptyPostParamTest() throws Exception {
+
+        this.mockMvc.perform(post("/")).andDo(print()).andExpect(status().is(400));
+    }
+}
